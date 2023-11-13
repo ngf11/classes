716739c0a1605a0dd619c7a5ac0925f100535855
Nicolas Fuentes <nfuentes11@gmail.com>
2023-11-13 14:08:18 -0500

    classes getter and setter

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
 
-  set name(value) {
-    this._name = value;
-  }
-
-  get name() {
-    return this._name.toUpperCaser();
-  }
-}
-
-const nico = new Person("nico");
-// console.log(nico);
-
-console.log(nico);
