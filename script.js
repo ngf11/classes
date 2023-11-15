4ac294adfa34935d7eff329594e8811fadcdedad
Nicolas Fuentes <nfuentes11@gmail.com>
2023-11-15 14:04:04 -0500

    classes

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  intro() {
-    return `Hi my name is ${this.name}, I Am ${this.age} years old`;
+class User {
+  name = "John";
+
+  sayHi() {
+    console.log(`Hello, ${this.name}!`);
   }
 }
 
-const nico = new Person("nico", 34);
-console.log(nico.intro());
-console.log(Person.prototype);
-console.log("-----");
-console.log(typeof Person);
-console.log("-----");
-console.log(Person === Person.prototype.constructor);
-console.log("-----");
-console.log(Person.prototype.sayHi);
-console.log("-----");
-console.log(Object.getOwnPropertyNames(Person.prototype));
+new User().sayHi();
