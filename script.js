1e5bc15d7c23b807ef9727318462fccc95c561f4
Nicolas Fuentes <nfuentes11@gmail.com>
2023-11-19 11:51:38 -0500

    getters and setters

--- script.js
+++ script.js
@@ -1,54 +1,24 @@
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-  }
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = "0" + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = "0" + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = "0" + secs;
-
-    let output = template
-      .replace("h", hours)
-      .replace("m", mins)
-      .replace("s", secs);
-
-    console.log(output);
-  }
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = "0" + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = "0" + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = "0" + secs;
-
-    let output = this.template
-      .replace("h", hours)
-      .replace("m", mins)
-      .replace("s", secs);
-
-    console.log(output);
-  }
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
-  }
-}
-
-let clock = new Clock({ template: "h:m:s" });
-clock.start();
+const person = {
+  name: "Nico",
+  skill: "Developer",
+  interest: ["fitness", "3D Animation"],
+  get intro() {
+    console.log(
+      `Hi, my name is ${this.name}. I am a ${this.skill}. My interest are: ${this.interest}`
+    );
+  },
+  set setInterest(val) {
+    this.interest.unshift(val);
+  },
+  set newName(val) {
+    this.name = val;
+  },
+  set setSkill(val) {
+    this.skill = val;
+  },
+};
+person.newName = "DiDi Wu";
+person.setSkill = "Astronaut";
+person.setInterest = "Space travel";
+person.setInterest = "Playing Futbol";
+person.intro;
