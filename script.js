965b65a1ea2100f04453a1924fe150fb6ef61c3d
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-17 11:17:43 -0500

    classes

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
-// Parent class super class
-class Pizza {
-  crust = "Orinal";
-  #sauce = "Traditional";
-  #size;
-  constructor(pizzaSize) {
-    this.#size = pizzaSize;
-  }
-  get pizzaCrust() {
-    return this.crust;
-  }
+const obj = {
+  greet: "Hi",
+  name: "nico",
+  get fullname() {
+    return `${this.greet} ${this.name}`;
+  },
+  set fullname(name) {
+    this.name = name;
+  },
+};
 
-  set pizzaCrust(val) {
-    this.crust = val;
-  }
-  order() {
-    console.log(
-      `Your order ${this.#size}, ${this.crust}, with ${this.#sauce} pizza`
-    );
-  }
-}
+console.log(obj);
+console.log(obj.fullname);
+obj.fullname = "Didi";
+console.log(obj.fullname);
 
-const myPizza = new Pizza("large");
-myPizza.pizzaCrust = "thin";
-myPizza.order();
+const user = {
+  firstname: "nico",
+  lastname: "fuentes",
+  get fullname() {
+    return `${this.firstname} ${this.lastname}`;
+  },
+  set fullname(value) {
+    [this.firstname, this.lastname] = value.split(" ");
+  },
+};
+
+console.log(user);
+console.log(user.fullname);
+user.fullname = "Didi Wu";
+console.log(user.fullname);
+console.log(user.firstname);
+console.log(user.lastname);
+
+const users = {
+  firstname: "siggy",
+  lastname: "siggy",
+  get fullname() {
+    return `${this.firstname} ${this.lastname}`;
+  },
+  set fullname(value) {
+    [this.firstname, this.lastname] = value.split(" ");
+  },
+};
+
+Object.defineProperty(users, "fullname", {
+  get() {
+    return `${this.firstname} ${this.lastname}`;
+  },
+  set(value) {
+    [this.firstname, this.lastname] = value.split(" ");
+  },
+});
+
+console.log(users);
+console.log(users.fullname);
+users.fullname = "Didi Wu";
+console.log(users.fullname);
+console.log(users.firstname);
+console.log(users.lastname);
