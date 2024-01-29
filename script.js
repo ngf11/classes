a813788157da3d0c842ca95b8574beb2be6e3125
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-28 21:25:32 -0500

    Setters and Getters

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
-class Clock {
-  constructor({ template }) {
-    this.template = template;
+class Pizza {
+  constructor(size, topping) {
+    this.size = size;
+    this.topping = topping;
+    this.crust = "Orignal";
   }
-
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = "0" + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = "0" + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = "0" + secs;
-
-    let output = this.template
-      .replace("h", hours)
-      .replace("m", mins)
-      .replace("s", secs);
-
-    console.log(output);
+  getPizza() {
+    console.log(
+      `You order a ${this.size}, ${this.topping} pizza with ${this.crust} crust`
+    );
   }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
+  setCrust(value) {
+    this.crust = value;
   }
 }
 
-let clock = new Clock({ template: "h:m:s" });
-clock.start();
+const myPie = new Pizza("Large", "Pepperoni");
+console.log(myPie.getPizza());
+myPie.setCrust("Thin");
+console.log(myPie.getPizza());
